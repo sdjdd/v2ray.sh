43732d5b5ebec1b3679929d1f387ee10d1d5fed2
sdjdd <i@sdjdd.com>
2019-05-28 18:26:38 +0800

    Support switch system proxy state

--- v2ray.sh
+++ v2ray.sh
@@ -3,20 +3,36 @@
 working_dir=`dirname $0`
 pid_file=$working_dir/v2ray.pid
 config='router'
+proxy_types=(
+    "web"
+    "secureweb"
+    "socksfirewall"
+)
 
 start() {
     if [[ -f $pid_file ]]; then
         kill `cat $pid_file`
+    else
+        for type in ${proxy_types[@]}; do
+            networksetup -set${type}proxystate Wi-Fi on
+        done
     fi
+
     $v2ray_path/v2ray -config $working_dir/$config.json >> /dev/null &
     echo -n $! > $pid_file
-    echo "v2ray run as $config" 
+
+    echo "v2ray run as $config"
 }
 
 stop() {
     if [[ -f $pid_file ]]; then
         kill `cat $pid_file`
         rm $pid_file
+
+        for type in ${proxy_types[@]}; do
+            networksetup -set${type}proxystate Wi-Fi off
+        done
+
         echo 'v2ray stopped'
     else
         echo 'v2ray is not running'
